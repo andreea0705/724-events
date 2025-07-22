c4b41f3d133acada691238b31219323194be32ce
Andreea Taflan <andreea07.05.2001@gmail.com>
2025-07-22 15:56:12 +0200

    resoudre l'image de footer en affichant la derniere image

--- Debuggez-une-application-React.JS-main/src/components/EventCard/index.js
+++ Debuggez-une-application-React.JS-main/src/components/EventCard/index.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { getMonth } from "../../helpers/Date";
-
+import { useData } from "../../contexts/DataContext";
 import "./style.scss";
 
 const EventCard = ({
@@ -11,22 +11,35 @@ const EventCard = ({
   label,
   small = false,
   ...props
-}) => (
+}) => {
+  const { data } = useData();
+  console.log(data?.events?.[data.events.length - 1]?.cover);
+  
+  const defaultImageSrc = data?.events?.[data.events.length - 1]?.cover || "";
+  const defaultImageAlt = data?.events?.[data.events.length - 1]?.description || "";
+  const defaultTitle = data?.events?.[data.events.length - 1]?.title || "";
+  
+  return(
     <div
       data-testid="card-testid"
       className={`EventCard${small ? " EventCard--small" : ""}`}
       {...props}
     >
       <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+        <img 
+          data-testid="card-image-testid" 
+          src={imageSrc || defaultImageSrc} 
+          alt={imageAlt || defaultImageAlt} 
+        />
         <div className="EventCard__label">{label}</div>
       </div>
       <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
+        <div className="EventCard__title">{title || defaultTitle}</div>
         <div className="EventCard__month">{getMonth(date)}</div>
       </div>
     </div>
   );
+}
 
 EventCard.propTypes = {
   imageSrc: PropTypes.string.isRequired,
